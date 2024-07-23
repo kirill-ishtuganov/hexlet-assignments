f36b95f88b88b31b317b22e42a182c9a017378b8
@hexlet/cli <support@hexlet.io>
2024-07-23 18:01:41 +0300

    submit java-web-ru/crud

---
+++ java-web-ru/crud/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,17 @@
+package exercise.controller;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+
+import exercise.dto.posts.PostsPage;
+import exercise.dto.posts.PostPage;
+import exercise.repository.PostRepository;
+
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+
+public class PostsController {
+
+    // BEGIN
+    
+    // END
+}
