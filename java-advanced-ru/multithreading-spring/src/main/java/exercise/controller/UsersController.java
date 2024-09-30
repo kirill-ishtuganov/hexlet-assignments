4ee8c76dad8d4ff1706ce52b8720f68dd9efe3f5
@hexlet/cli <support@hexlet.io>
2024-09-30 17:25:01 +0300

    submit java-advanced-ru/multithreading-spring

---
+++ java-advanced-ru/multithreading-spring/src/main/java/exercise/controller/UsersController.java
@@ -0,0 +1,53 @@
+package exercise.controller;
+
+import exercise.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import exercise.service.UserService;
+
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(path = "")
+    public Flux<User> getUsers() {
+        return userService.findAll();
+    }
+
+    // BEGIN
+    @GetMapping("/{id}")
+    public Mono<User> show(@PathVariable int id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping("")
+    public Mono<User> create(@RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @PatchMapping("/{id}")
+    public Mono<User> updste(@PathVariable int id, @RequestBody User user) {
+        return userService.update(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> delete(@PathVariable int id) {
+        return userService.delete(id);
+    }
+    // END
+}
